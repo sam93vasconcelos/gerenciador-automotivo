11596ae04c39a1b6035cb5bb802e50b3010316fd
= <=>
2021-10-04 23:51:20 -0300

    Remoção de veículo

--- src/pages/ShowVehicle/Index.js
+++ src/pages/ShowVehicle/Index.js
@@ -1,18 +1,21 @@
 import { useState, useEffect } from 'react';
 import http from '../../services/http';
-import { FaPencilAlt } from 'react-icons/fa';
+import { FaPencilAlt, FaTrashAlt } from 'react-icons/fa';
 import LoadingOverlay from '../../components/loadingOverlay/Index';
 import { Link } from 'react-router-dom';
 import NewSupply from '../NewSupply/Index';
 import UpdateVehicle from '../UpdateVehicle/Index';
 
 import './styles.scss';
+import toast from 'react-hot-toast';
+import history from '../../services/history';
 
 function ShowVehicle(props) {
 	const [ vehicle, setVehicle ] = useState();
+	const [ loading, setLoading ] = useState(false);
 	const { id } = props.match.params;
 
-	useEffect(() => {	
+	useEffect(() => {
 		getVehicle()
 	}, []);
 
@@ -28,24 +31,49 @@ function ShowVehicle(props) {
     setVehicle(vehicle);
   }
 
+	async function deleteVehicle() {
+		if(window.confirm('Tem certeza que deseja excluir este veículo e todos os seus movimentos? Esta ação não pode ser desfeita')) {
+			setLoading(true);
+			try {
+				await http.delete(`vehicles/${vehicle.id}`);
+				toast.success('Removido!');
+				history.push('/');
+			} catch (error) {
+				console.log(error);
+				toast.error('Erro ao remover veículo');
+			}
+			setLoading(false);
+		}
+	}
+
 	return (
 		<div>
-			{vehicle ? '' : <LoadingOverlay />}
+			{vehicle && !loading ? '' : <LoadingOverlay />}
+
 			<div className="container">
 				<NewSupply fn={fn} />
+
 				<UpdateVehicle 
 					handleUpdatedVehicle={ handleUpdatedVehicle } 
 					fn={fn} 
 					vehicle={ vehicle } 
 				/>
 				
-				<h1 className="text-center">
-					Detalhes | {vehicle?.name} <FaPencilAlt
-																			size={25} 
-																			color="#dd5" 
-																			onClick={() => fn.handleShow(true)}
-																			className="cursor-pointer"
-																		/>
+				<h1 className="text-center vehicle-actions">
+					<span>Detalhes |</span>
+					<span>{vehicle?.name}</span>
+					<FaPencilAlt
+						size={25} 
+						color="#dd5" 
+						onClick={() => fn.handleShow(true)}
+						className="cursor-pointer"
+					/>
+					<FaTrashAlt 
+						size={25} 
+						color="#d55" 
+						onClick={deleteVehicle}
+						className="cursor-pointer"
+					/>
 				</h1>
 
 				<section className="show-vehicles-container">
